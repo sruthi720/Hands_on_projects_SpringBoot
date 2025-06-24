d8f13e58a965132ff5fa8e574a011c530a36a42c
Sruthi.I.R <sruthiir720@gmail.com>
2025-06-24 12:24:07 +0530

    todo app completed

--- simple 6 SpringBoot projects/todoApp/src/main/java/com/app/todoApp/models/Task.java
+++ simple 6 SpringBoot projects/todoApp/src/main/java/com/app/todoApp/models/Task.java
@@ -4,14 +4,40 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
 
 @Entity
-@Data
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String title;
-    private boolean compleated;
+
+    private boolean completed;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+// You can remove @Data for now to test
 }
+
